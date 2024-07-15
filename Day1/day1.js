5d4e2689b12bc9e23c49fbf82b6b06c9a1ce8b0b
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-07-15 15:33:39 +0530

    This is 30 days of JS Challenge Day One

---
+++ Day1/day1.js
@@ -0,0 +1,45 @@
+//activity 1
+
+var apple =10;
+console.log(apple);
+let mango = "I like to eat Mangoes";
+console.log(mango);
+
+
+//activity 2
+
+const iLoveMangoes = true;
+console.log(iLoveMangoes);
+
+//Activity 3
+
+let a =23;
+console.log(typeof(a));
+
+
+let b="Thus is a string";
+console.log(typeof(b));
+
+
+
+let c= true;
+console.log(typeof(c));
+
+
+let d={firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(typeof(d));
+
+const e = [2,3,5,7,9,12];
+console.log(typeof(e));
+
+
+
+
+//activity 4
+
+b =21;
+console.log(b);
+
+
+//activity 5
+e=32;//here it throw error because e is a constant
