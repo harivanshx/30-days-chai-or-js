f524b4cef670218c67a0fee2e832d39b64b5d716
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-07-16 20:25:18 +0530

    day 2

---
+++ Day2/day2.js
@@ -0,0 +1,69 @@
+//Task 1
+let a=12;
+let b=34;
+console.log(a+b);
+
+//Task 2
+let c=20;
+let d=13;
+console.log(c-d);
+
+//Task 3
+let e=23;
+let f=2;
+console.log(e*f);
+
+//Task 4
+let g=34;
+let h=12;
+console.log(g/h);
+
+//Task 5
+let i=14;
+let j=3;
+console.log(i%j);
+
+//Activity 2
+
+// Task 6
+let k=12;
+k+=12;
+console.log(k);
+
+//Task 7
+let l=13;
+l-=3;
+console.log(l);
+
+//Activity 3
+
+//Task 8
+console.log(k>a);
+
+//Task 9
+console.log(k<a);
+
+//Task 10
+console.log(a==b);
+
+//Activity 4
+
+// Task11
+tr=a==12&&b==34;
+console.log(tr);
+
+// Task12
+fl=a==12||b==13;
+console.log(fl);
+
+// Task13
+no=a!=b;
+console.log(no);
+
+// Activity 5
+
+// Task 14
+
+const age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice";
+console.log(beverage); // "Beer"
