7d16e5cce778f9cfb170d51036fd013fc0addc5b
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2024-07-18 23:45:32 +0530

    Day 5

--- Day 4/day4.js
+++ Day 4/day4.js
@@ -1,4 +1,4 @@
-Activity 1: For Loop
+// Activity 1: For Loop
 
 // Task1
 let a=0;
@@ -16,10 +16,10 @@ for (let num = 0; num <= 100; num=num+5) {
 }
 
 
-Activity 2
+// Activity 2
 
-Task 3
-sum of 1 to n
+// Task 3
+// sum of 1 to n
 
 let n=10;
 for (let i = 1,sum=1; i < sum; i++) {
@@ -34,8 +34,42 @@ console.log(sum);
 //Task 4
 // 10 to 1
 
-for (let i = 10; i >=1 ; i--) {
-    console.log(i);
+for (let j = 10; j >=1 ; j--) {
+    console.log(j);
 
     
 }
+// Activity 3
+// Task5
+let apple = 1;
+do {let apple = 1;
+    console.log(apple);
+    apple++;
+
+} while (apple > 10);
+
+// Task 6
+//factorial using do while loop
+// fact=10*9*8*7*6*5*4*3*2*1
+
+num=1;
+fact=1;
+
+
+do {
+    fact=fact*num;
+    num++;
+if(num==10){
+    console.log(fact);
+}
+} while (num<=10);
+//Activity 4
+// Task6
+for (let i = 0; i < n; i++) {
+    for (let k = 0; k < n; k++) {
+        console.log(" * ");
+    } console.log("\n");
+   
+    
+}
+// pattern
